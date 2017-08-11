f908b187101370933e30de9d4c77bd77d7f5b0b5
Benjamin Lucas <lucas.banet@outlook.com>
2017-08-10 22:59:32 -0400

    E38_IncomeTax_1

--- U9/Form1.Designer.vb
+++ U9/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frm_IncomeTaxCalc
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,79 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.txt_IncomeAmt = New System.Windows.Forms.TextBox()
+        Me.lbl_IncomeAmt = New System.Windows.Forms.Label()
+        Me.btn_CalcTax = New System.Windows.Forms.Button()
+        Me.lbl_TaxesDue = New System.Windows.Forms.Label()
+        Me.txt_TaxTotal = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'txt_IncomeAmt
+        '
+        Me.txt_IncomeAmt.AcceptsReturn = True
+        Me.txt_IncomeAmt.AcceptsTab = True
+        Me.txt_IncomeAmt.Location = New System.Drawing.Point(105, 9)
+        Me.txt_IncomeAmt.MaxLength = 15
+        Me.txt_IncomeAmt.Name = "txt_IncomeAmt"
+        Me.txt_IncomeAmt.Size = New System.Drawing.Size(111, 20)
+        Me.txt_IncomeAmt.TabIndex = 0
+        Me.txt_IncomeAmt.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lbl_IncomeAmt
+        '
+        Me.lbl_IncomeAmt.AutoSize = True
+        Me.lbl_IncomeAmt.Location = New System.Drawing.Point(24, 12)
+        Me.lbl_IncomeAmt.Name = "lbl_IncomeAmt"
+        Me.lbl_IncomeAmt.Size = New System.Drawing.Size(75, 13)
+        Me.lbl_IncomeAmt.TabIndex = 1
+        Me.lbl_IncomeAmt.Text = "Total Income :"
+        '
+        'btn_CalcTax
+        '
+        Me.btn_CalcTax.Location = New System.Drawing.Point(27, 50)
+        Me.btn_CalcTax.Name = "btn_CalcTax"
+        Me.btn_CalcTax.Size = New System.Drawing.Size(189, 20)
+        Me.btn_CalcTax.TabIndex = 2
+        Me.btn_CalcTax.Text = "Calculate Tax"
+        Me.btn_CalcTax.UseVisualStyleBackColor = True
+        '
+        'lbl_TaxesDue
+        '
+        Me.lbl_TaxesDue.AutoSize = True
+        Me.lbl_TaxesDue.Location = New System.Drawing.Point(24, 93)
+        Me.lbl_TaxesDue.Name = "lbl_TaxesDue"
+        Me.lbl_TaxesDue.Size = New System.Drawing.Size(73, 13)
+        Me.lbl_TaxesDue.TabIndex = 4
+        Me.lbl_TaxesDue.Text = "Taxes Owed :"
+        '
+        'txt_TaxTotal
+        '
+        Me.txt_TaxTotal.Location = New System.Drawing.Point(105, 90)
+        Me.txt_TaxTotal.MaxLength = 15
+        Me.txt_TaxTotal.Name = "txt_TaxTotal"
+        Me.txt_TaxTotal.Size = New System.Drawing.Size(111, 20)
+        Me.txt_TaxTotal.TabIndex = 3
+        Me.txt_TaxTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'frm_IncomeTaxCalc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
-    End Sub
+        Me.ClientSize = New System.Drawing.Size(240, 119)
+        Me.Controls.Add(Me.lbl_TaxesDue)
+        Me.Controls.Add(Me.txt_TaxTotal)
+        Me.Controls.Add(Me.btn_CalcTax)
+        Me.Controls.Add(Me.lbl_IncomeAmt)
+        Me.Controls.Add(Me.txt_IncomeAmt)
+        Me.Name = "frm_IncomeTaxCalc"
+        Me.Text = "Income Tax"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
+    End Sub
+    Friend WithEvents lbl_IncomeAmt As Label
+    Friend WithEvents btn_CalcTax As Button
+    Friend WithEvents lbl_TaxesDue As Label
+    Friend WithEvents txt_TaxTotal As TextBox
+    Friend WithEvents txt_IncomeAmt As TextBox
 End Class
--- U9/Form1.vb
+++ U9/Form1.vb
@@ -1,3 +1,13 @@
-﻿Public Class Form1
+﻿Public Class frm_IncomeTaxCalc
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lbl_IncomeAmt.Click
 
+    End Sub
+
+    Private Sub btn_CalcTax_Click(sender As Object, e As EventArgs) Handles btn_CalcTax.Click
+
+    End Sub
+
+    Private Sub txt_IncomeAmt_TextChanged(sender As Object, e As EventArgs) Handles txt_IncomeAmt.TextChanged
+
+    End Sub
 End Class
